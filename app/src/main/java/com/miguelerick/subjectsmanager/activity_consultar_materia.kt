a9f8ebc344255e4f37ad3ab9fde9c439e2f5f0ba
Erick Pinzon <erickpinzonhuerta@gmail.com>
2022-11-22 20:40:50 -0600

    Layouts Created

---
+++ app/src/main/java/com/miguelerick/subjectsmanager/activity_consultar_materia.kt
@@ -0,0 +1,11 @@
+package com.miguelerick.subjectsmanager
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class activity_consultar_materia : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_consultar_materia)
+    }
+}
---
+++ app/src/main/java/com/miguelerick/subjectsmanager/activity_detalles_materia.kt
@@ -0,0 +1,11 @@
+package com.miguelerick.subjectsmanager
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class activity_detalles_materia : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detalles_materia)
+    }
+}
